3db24ef3a9ad9495910ee208d94c4beb1537ef35
kimathinjoki <dfkimathinjoki@gmail.com>
2022-12-15 12:06:02 +0300

    added the quiz questions

--- index.js
+++ index.js
@@ -19,8 +19,24 @@ function questionFetch(){
     .then(res =>res.json())
     .then(data =>{
         console.log(data)
-        data.forEach(item => {
-            console.log(item.question)
+        data.forEach((item,i)=> {
+            // console.log(item.question)
+            const qn = document.getElementById("question")
+            const rightAnswer = document.getElementById("answer")
+            // const rightAnswr = document.createElement("li")
+
+            setTimeout(() => {
+                console.log(item.question);
+                qn.innerText=item.question;
+                rightAnswer.innerText = item.correctAnswer
+                // rightAnswer.appendChild(rightAnswr)
+
+            }, i * 9000)
+            setTimeout(()=>{
+                console.log(item.correctAnswer)
+                // rightAnswr.innerText = item.correctAnswer
+                // rightAnswer.appendChild(rightAnswr)
+            },i*6000)
 
         });
     })
