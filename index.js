1bd6a564d021deb7b75d9fc1fc6cdbc370d5df1f
kimathinjoki <dfkimathinjoki@gmail.com>
2022-12-15 09:59:35 +0300

    added get requiest

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
 document.addEventListener("DOMContentLoaded",()=>{
+    questionFetch()
 
 
 
 })
 
+const randomQn = "https://the-trivia-api.com/api/questions";
+// const configData ={
+//     method:"GET"
+//     headers:{
+//         "Content-Type":"application/json"
+//         Accept: "application/json"
+//     }
+// }
+
 function questionFetch(){
-    fetch
+    fetch(randomQn)
+    .then(res =>res.json())
+    .then(data =>{
+        console.log(data)
+        data.forEach(item => {
+            console.log(item.question)
+
+        });
+    })
 }
