84f120c616cdb40e087f9937b13b3c1ae5391076
kimathinjoki <dfkimathinjoki@gmail.com>
2022-12-16 16:40:20 +0300

    added replay button

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ const music = "https://the-trivia-api.com/api/questions?categories=music&limit=2
 const science = "https://the-trivia-api.com/api/questions?categories=science&limit=20";
 const society = "https://the-trivia-api.com/api/questions?categories=society_and_culture&limit=20";
 const sport = "https://the-trivia-api.com/api/questions?categories=sport_and_leisure&limit=20";
-const randomQn = "https://the-trivia-api.com/api/questions";
+const randomQn = "https://the-trivia-api.com/api/questions?categories=arts_and_literature,film_and_tv,food_and_drink,general_knowledge,geography,history,music,science,society_and_culture,sport_and_leisure&limit=20";
 
-//getting the specific category menus
+//getting the specific category menus and elements
 
 const artElem = document.getElementById("art");
 const filmElem = document.querySelector("#film");
@@ -23,6 +23,10 @@ const musicElem = document.getElementById("music");
 const scienceElem = document.getElementById("science");
 const societyElem = document.getElementById("society");
 const sportElem = document.getElementById("sport");
+
+const triviaCont = document.querySelector(".triviaContainer")
+const replaybtn = document.querySelector(".replay")
+const body = document.querySelector("body")
 
 //the start button function
 const startbtn = document.querySelector(".start")
@@ -45,6 +49,11 @@ document.addEventListener("DOMContentLoaded",()=>{
 
     })
 
+    //initiating a replay of a random round
+    replaybtn.addEventListener('click',()=>{
+        questionFetch(randomQn)
+    })
+
 // diplay register form when pressed.
     const registerBtn = document.querySelector(".register")
     const form = document.querySelector("#regForm")
@@ -53,7 +62,21 @@ document.addEventListener("DOMContentLoaded",()=>{
         form.style.display="block"
 
     })
-    loginBtn.addEventListener('click')
+    loginBtn.addEventListener('click',()=>{
+        form.style.display ="none"
+    })
+    //on pressing enter
+    login.addEventListener("keypress",(event)=>{
+        if (event.key === "Enter") {
+            form.style.display = "none"
+        }
+      });
+
+      // on clicking anywhere in the body
+    //   body.addEventListener('click',()=>{
+
+    //     form.style.display = "none"
+    //   })
 
 // function to hide the start button when a category is clicked
     const hideStart = function(){
@@ -180,8 +203,7 @@ function questionFetch(url){
 
                 //making the progress bar restart on each loop
                 progressBar.style.animation ="load 10s";
-                progressBar.style= "animation-iteration-count:infinite";
-
+                progressBar.style= "animation-iteration-count: 21";
 
             }, i * 10000)
 
